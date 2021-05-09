d5e33655fb8b584d17b2109b121c81762e6dd0fc
aptennn <uegor@protonmail.com>
2021-05-09 14:57:55 +0300

    ui2

--- app/src/main/java/com/example/samsunghackathon2021/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/samsunghackathon2021/ui/profile/ProfileFragment.java
@@ -11,14 +11,20 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.samsunghackathon2021.R;
+import com.example.samsunghackathon2021.State;
+import com.example.samsunghackathon2021.StateAdapter;
 import com.example.samsunghackathon2021.databinding.FragmentProfileBinding;
+
+import java.util.ArrayList;
 
 public class ProfileFragment extends Fragment {
 
     private ProfileViewModel profileViewModel;
     private FragmentProfileBinding binding;
-
+    ArrayList<State> states = new ArrayList<State>();
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         profileViewModel =
@@ -26,15 +32,24 @@ public class ProfileFragment extends Fragment {
 
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        setInitialData();
+        RecyclerView recyclerView = (RecyclerView) root.findViewById(R.id.list);
+        // создаем адаптер
+        StateAdapter adapter = new StateAdapter(getContext(), states);
+        // устанавливаем для списка адаптер
+        recyclerView.setAdapter(adapter);
 
-        final TextView textView = binding.textGallery;
-        profileViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
+    }
+
+    private void setInitialData(){
+
+        states.add(new State ("Бразилия", "Бразилиа", R.drawable.baseline_home_24));
+        states.add(new State ("Аргентина", "Буэнос-Айрес", R.drawable.baseline_house_siding_24));
+        states.add(new State ("Колумбия", "Богота", R.drawable.baseline_agriculture_24));
+        states.add(new State ("Уругвай", "Монтевидео", R.drawable.baseline_window_24));
+        states.add(new State ("Чили", "Сантьяго", R.drawable.ic_baseline_ac_unit_24));
     }
 
     @Override
